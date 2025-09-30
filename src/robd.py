42bc58a96740a128c6786b375a9a5d4571c1a9a4
lune45 <145884764+lune45@users.noreply.github.com>
2025-09-30 11:51:11 +0800

    finish writing preprocess.py, produce robd with robd

--- src/robd.py
+++ src/robd.py
@@ -8,7 +8,17 @@ def robd_single_sequence(y_seq, m=5, la1=1, la2=0):
     """
     x = np.zero_like(y_seq, dtype=float)
     x[0] = y_seq[0]
-    denom = m + 2 * la1 +la2
+    denom = m + 2 * la1 + la2
     for t in range(1, len(y_seq)):
-        x[t] =(m * y_seq[t] + 2* la1 *x[t - 1])/ denom
+        x[t] =(m * y_seq[t] + 2 * la1 * x[t - 1]) / denom
     return x
+
+def compute_cost(x_seq, y_seq, m=5.0):
+    hitting = (m/2.0) * np.sum((x_seq - y_seq) ** 2)
+
+    if len(x_seq) > 1:
+        switching = 0.5 * np.sum((x_seq[1:] - x_seq[:-1]) ** 2)
+
+    else:
+        switching = 0.0
+    return hitting + switching
